9783186db8dfff3ed753bd1b1db555d8a827ad54
RicardoBS04 <j.bolanos.2@estudiantec.cr>
2025-10-22 19:42:39 -0600

    Agregué varios metodos

--- proyecto.py
+++ proyecto.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import *
+import random
 
 def juego():
     #ventana_principal.withdraw()
@@ -12,19 +13,19 @@ def juego():
 
     ventana_juego.geometry(f"{ancho_ventana}x{alto_ventana}")
 
-    ancho_canvas = int(ancho_ventana * 0.8) // 40 * 40
-    alto_canvas = int(alto_ventana * 0.8) // 40 * 40
+    ancho_can = int(ancho_ventana * 0.8) // 40 * 40
+    alto_can = int(alto_ventana * 0.8) // 40 * 40
 
-    print(ancho_canvas)
-    print(alto_canvas)
+    print(ancho_can)
+    print(alto_can)
 
-    canvas_juego = Canvas(ventana_juego, width=ancho_canvas, height=alto_canvas, bg="black")
-    canvas_juego.pack()
+    can_juego = Canvas(ventana_juego, width=ancho_can, height=alto_can, bg="black")
+    can_juego.pack()
 
     
-    canvas_juego.jugador = crear_personaje(canvas_juego, 640, 360)
-    canvas_juego.bind_all("<KeyPress>", lambda event: mover_jugador(canvas_juego, event, 20, ancho_canvas, alto_canvas))
-    canvas_juego.ultima_direccion = "arriba"
+    can_juego.jugador = crear_personaje(can_juego, 640, 360)
+    can_juego.bind_all("<KeyPress>", lambda event: mover_jugador(can_juego, event, 20, ancho_can, alto_can))
+    can_juego.ultima_direccion = "arriba"
 
 #Bloque de funciones 
 #################################################################################
@@ -40,7 +41,7 @@ def crear_personaje(can, x, y):
 
 #Mover Jugador
 ######################################################################
-def mover_jugador(can, event, velocidad, ancho_canvas, alto_canvas):
+def mover_jugador(can, event, velocidad, ancho_can, alto_can):
     movimiento_x = 0
     movimiento_y = 0
 
@@ -66,9 +67,11 @@ def mover_jugador(can, event, velocidad, ancho_canvas, alto_canvas):
     ny = y + movimiento_y"""
 
     can.move(can.jugador, movimiento_x, movimiento_y)
-    fuera_pantalla(can, can.jugador, ancho_canvas, alto_canvas)
+    fuera_pantalla(can, can.jugador, ancho_can, alto_can)
 
 
+#Función para detectar que salió fuera de la pantalla el jugador
+#############################################################################
 def fuera_pantalla(can, objeto, ancho, alto):
     x1, y1, x2, y2 = can.coords(objeto)
     ancho_objeto = x2 - x1
@@ -86,6 +89,80 @@ def fuera_pantalla(can, objeto, ancho, alto):
     elif y1 > alto:
         can.coords(objeto, x1, 0, x2, alto_objeto)
 
+
+#Función para crear a los personajes
+##############################################################################
+def crear_enemigos(can, nivel):
+
+    x = random.randint(40, 1160)
+    y = random.randint(40, 640)
+
+    if nivel == 1:
+        imagen = can.imagen_rosa1
+        velocidad = 5
+    elif nivel == 2:
+        imagen = can.imagen_azul1
+        velocidad = 5
+    elif nivel == 3:
+        imagen = can.imagen_verde1
+        velocidad = 5
+    else:
+        return 
+
+    can.moto = crear_personaje(can, x, y, imagen)
+    
+    return can.moto
+    
+    """if random.choice([True, False]):
+        vel_x = random.choice([-velocidad, velocidad])
+        vel_y = 0
+    else:
+        vel_x = 0
+        vel_y = random.choice([-velocidad, velocidad])"""
+
+#Función para mover la moto
+########################################################################
+"""def mover_moto(can, moto_1, moto_2, moto_3, x, y):
+
+    can.move(moto_1, x, y)
+    can.move(moto_2, x, y)
+    can.move(moto_3, x, y)
+
+
+    pos_moto1 = can.coords(bola_roja)
+    pos_moto2 = can.coords(bola_azul)
+
+    #Valorar si las esferas rebotan en los bordes
+
+    #Bola Roja
+    if pos_moto1[3] >= 300 or pos_moto1[1] <= 0:
+        ventana.vel_rojo[1] = -ventana.vel_rojo[1]
+
+    if pos_moto1[2] >= 400 or pos_moto1[0] <= 0:
+        ventana.vel_rojo[0] = -ventana.vel_rojo[0]
+
+
+    #Bola azul
+    if pos_moto2[3] >= 300 or pos_moto2[1] <= 0:
+        ventana.vel_azul[1] = -ventana.vel_azul[1]
+        
+    if pos_moto2[2] >= 400 or pos_moto2[0] <= 0:
+        ventana.vel_azul[0] = -ventana.vel_azul[0]
+
+
+    #Valorar si las esferas rebotan entre ellas
+
+    if (pos_moto1[0] < pos_moto2[2] and
+        pos_moto1[2] > pos_moto2[0] and
+        pos_moto1[1] < pos_moto2[3] and
+        pos_moto1[3] > pos_moto2[1]):
+
+        ventana.vel_rojo[0] = -ventana.vel_rojo[0]
+        ventana.vel_rojo[1] = -ventana.vel_rojo[1]
+        ventana.vel_azul[0] = -ventana.vel_azul[0]
+        ventana.vel_azul[1] = -ventana.vel_azul[1]
+
+    ventana.after(20, AnimacionRecursiva, can, bola_roja, bola_azul, ventana)"""
 
 #Ventana Principal
 #########################################################################################
